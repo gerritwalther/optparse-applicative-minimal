c2034cfe17bafa5055373d2cc89e066a945cf72e
Gerrit Walther <gerritwalther@gmail.com>
2015-07-26 14:12:19 +0200

    Added files for stack/cabal and implemented first (not working) version of  the minimal example.

---
+++ src/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Options.Applicative
+import Control.Monad.Trans.Reader
+
+data Configuration = Configuration
+                    { foo :: String
+                    , bar :: Int
+                    }
+
+configuration :: Parser Configuration
+configuration = Configuration
+    <$> strOption
+        ( long "foo"
+       <> metavar "ARG1"
+        )
+    <*> option
+        ( long "bar"
+       <> metavar "ARG2"
+       )
+
+main :: IO ()
+main = do
+    config <- execParser (info configuration fullDesc)
+    putStrLn (show (bar config) ++ foo config)
